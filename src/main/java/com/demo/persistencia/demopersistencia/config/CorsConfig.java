887245e1645dda3366e527a84892d1602d90ba73
steveegt <jguineatzul@gmail.com>
2026-05-31 13:59:36 -0600

    VersionFINAL

--- src/main/java/com/demo/persistencia/demopersistencia/config/CorsConfig.java
+++ src/main/java/com/demo/persistencia/demopersistencia/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
 
         // ✅ ❗ SIN SLASH AL FINAL (ERROR QUE TENÍAS)
         config.setAllowedOrigins(List.of(
-            "https://frontend-proyecto-production-cce2.up.railway.app/"
+            "https://frontend-proyecto-production-6ef5.up.railway.app/"
         ));
 
         config.setAllowedHeaders(List.of("*"));
