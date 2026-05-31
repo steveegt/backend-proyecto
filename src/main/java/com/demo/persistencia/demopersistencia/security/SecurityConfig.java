9561540acfae849db5fb6e327d28f7d8a15fb3be
steveegt <jguineatzul@gmail.com>
2026-05-31 00:05:26 -0600

    fix security config allow login

--- src/main/java/com/demo/persistencia/demopersistencia/security/SecurityConfig.java
+++ src/main/java/com/demo/persistencia/demopersistencia/security/SecurityConfig.java
@@ -1,16 +1,31 @@
 package com.demo.persistencia.demopersistencia.security;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll() // ✅ LOGIN LIBRE
+                .anyRequest().permitAll() // ✅ TODO LIBRE (por ahora)
+            );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
+
+
